681126fe2d04d2d09c2e21f4b18e6f48cfe9feda
Dmytro V <staff1489@gmail.com>
2017-05-02 11:28:44 +0300

    Phone#mapping added

--- lib/phoneword/phone.rb
+++ lib/phoneword/phone.rb
@@ -4,5 +4,18 @@ module Phoneword
       @number = args[:number]
     end
 
+    def mapping
+      @mapping ||= {
+        2 => %w(a b c),
+        3 => %w(d e f),
+        4 => %w(g h i),
+        5 => %w(j k l),
+        6 => %w(m n o),
+        7 => %w(p q r s),
+        8 => %w(t u v),
+        9 => %w(w x y z)
+      }
+    end
+
   end
 end
